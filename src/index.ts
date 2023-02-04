35547dae24e124f30f5dd6187d986b95781993cc
Bodhan Shrestha <bodhanshrestha@gmail.com>
2023-02-04 20:50:46 +0545

    Multiple

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,19 @@ app.use(express.urlencoded({ extended: true }))
 
 app.post('/upload_files', upload.single('image'), uploadFiles)
 
+app.post(
+	'/upload_files/multiple',
+	upload.array('images', 5),
+	uploadFilesMultiple
+)
+
 function uploadFiles(req: Request, res: Response) {
 	console.log(req.body, req.file)
+	return res.json({ success: true })
+}
+
+function uploadFilesMultiple(req: Request, res: Response) {
+	console.log(req.body, req.files)
 	return res.json({ success: true })
 }
 app.listen(5000, () => {
