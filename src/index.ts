9a7e39aecb7e84b97ca1a965a4c93d0b261f2e46
Bodhan Shrestha <bodhan@noveltytechnology.com>
2023-02-05 19:38:10 +0545

    Add contoller multer from inside

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,42 @@
-import express, { Request, Response } from 'express'
-import upload from './multer'
+import express, { Request, Response } from "express";
+import upload, { uploadFile } from "./multer";
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/upload_files', upload.single('image'), uploadFiles)
+app.post("/upload_files", upload.single("image"), uploadFiles);
+
+app.post("/upload_files/inside", uploadFilesInside);
 
 app.post(
-	'/upload_files/multiple',
-	upload.array('images', 5),
-	uploadFilesMultiple
-)
+  "/upload_files/multiple",
+  upload.array("images", 5),
+  uploadFilesMultiple
+);
 
 function uploadFiles(req: Request, res: Response) {
-	console.log(req.body, req.file)
-	return res.json({ success: true })
+  console.log(req.body, req.file);
+  return res.json({ success: true });
+}
+function uploadFilesInside(req: Request, res: Response) {
+  //   upload.single("image")(req, res, () => {
+  //     console.log(req.body, req.files);
+  //     return res.json({ success: true });
+  //   });
+  uploadFile(req, res, (error) => {
+    if (error) {
+      return res.status(400).send(error);
+    }
+    console.log(req.body, req.files);
+    return res.json({ success: true });
+  });
 }
 
 function uploadFilesMultiple(req: Request, res: Response) {
-	console.log(req.body, req.files)
-	return res.json({ success: true })
+  console.log(req.body, req.files);
+  return res.json({ success: true });
 }
-app.listen(5000, () => {
-	console.log(`Server started...`)
-})
+app.listen(3000, () => {
+  console.log(`Server started...`);
+});
--- src/multer.ts
+++ src/multer.ts
@@ -1,27 +1,29 @@
-import express, { Request, Response, NextFunction } from 'express'
-import multer from 'multer'
+import express, { Request, Response, NextFunction } from "express";
+import multer from "multer";
 
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, './uploads/')
-	},
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
 
-	filename: function (req: any, file: any, cb: any) {
-		cb(null, file.originalname)
-	},
-})
+  filename: function (req: any, file: any, cb: any) {
+    cb(null, file.originalname);
+  },
+});
 const fileFilter = (req: any, file: any, cb: any) => {
-	console.log('running', file)
-	if (
-		file.mimetype === 'image/jpg' ||
-		file.mimetype === 'image/jpeg' ||
-		file.mimetype === 'image/png'
-	) {
-		cb(null, true)
-	} else {
-		cb(new Error('Image uploaded is not of type jpg/jpeg or png'), false)
-	}
-}
-const upload = multer({ storage: storage, fileFilter: fileFilter })
+  console.log("running", file);
+  if (
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Image uploaded is not of type jpg/jpeg or png"), false);
+  }
+};
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-export default upload
+export const uploadFile = upload.single("image");
+
+export default upload;
